be6accc7240f97d22f6435b8e59427111f0436b0
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-04 03:54:00 +0900

    chore: fix tiny CSS

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -18,7 +18,7 @@ const Card = ({ data, boardKey }: CardProps) => {
       </InfoContainer>
       {boardKey === 'toDo' ? (
         <BtnContainer>
-          <button>
+          <Btn>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -34,8 +34,8 @@ const Card = ({ data, boardKey }: CardProps) => {
                 d="M4.5 12.75l6 6 9-13.5"
               />
             </svg>
-          </button>
-          <button>
+          </Btn>
+          <Btn>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -51,12 +51,44 @@ const Card = ({ data, boardKey }: CardProps) => {
                 d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
               />
             </svg>
-          </button>
+          </Btn>
         </BtnContainer>
       ) : (
         <BtnContainer>
-          <button>rollback</button>
-          <button>delete</button>
+          <Btn>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+              width={30}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+              />
+            </svg>
+          </Btn>
+          <Btn>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+              width={30}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+          </Btn>
         </BtnContainer>
       )}
     </Wrapper>
@@ -77,7 +109,6 @@ const Wrapper = styled(motion.div)`
   justify-content: space-between;
 
   :hover {
-    scale: 1.025;
     opacity: 1;
     cursor: pointer;
   }
@@ -91,8 +122,21 @@ const InfoContainer = styled.div`
   margin-right: 10px;
 `;
 
+const Btn = styled.div`
+  padding: 5px;
+  border-radius: 3px;
+  transition: ease-in-out 0.15s;
+
+  :hover {
+    background: rgba(111, 111, 111, 0.6);
+  }
+`;
+
 const BtnContainer = styled.div`
-  background: tomato;
+  display: flex;
+  div {
+    margin: 3px;
+  }
 `;
 
 const Title = styled.div`
