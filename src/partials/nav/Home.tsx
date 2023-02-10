35eb4afe44dcfebf37516154c09962b3b7a50445
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-10 17:35:54 +0900

    feat: (Nav)Home버튼 svg 및 motion 추가

--- src/partials/nav/Home.tsx
+++ src/partials/nav/Home.tsx
@@ -1,11 +1,41 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <Link to="/">
-      <div>Home</div>
-    </Link>
+    <AnimatePresence>
+      <Link to="/">
+        <Svg
+          variants={svgVariants}
+          initial="from"
+          animate="to"
+          exit="exit"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+          width={30}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </Svg>
+      </Link>
+    </AnimatePresence>
   );
 };
 
 export default Home;
+
+const Svg = styled(motion.svg)``;
+
+const svgVariants = {
+  from: { opacity: 0 },
+  to: { opacity: 1, transition: { duration: 0.15 } },
+  exit: { opacity: 0, transition: { duration: 0.15 } },
+};
