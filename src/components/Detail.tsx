86ce71ee90aa50116383e2ea3d1d112162474bef
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-11 02:57:56 +0900

    feat: Detail Page Layout 작성

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
+import styled from 'styled-components';
 import { toDoDatasAtom } from '../atom';
+import { Link } from 'react-router-dom';
 
 const Detail = () => {
   const { taskId } = useParams();
@@ -11,12 +12,87 @@ const Detail = () => {
     .flat()[0];
 
   return (
-    <>
-      <div>{task.id}</div>
-      <div>{task.title}</div>
-      <div>{task.description}</div>
-    </>
+    <Container>
+      <Wrapper>
+        <Title>
+          <Text>title : {task.title}</Text>
+          <Link to="/">
+            <Svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+              width={30}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </Svg>
+          </Link>
+        </Title>
+        <Content>
+          <div>id : {task.id}</div>
+          <div>description : {task.description}</div>
+        </Content>
+      </Wrapper>
+    </Container>
   );
 };
 
 export default Detail;
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.div``;
+
+const Svg = styled.svg`
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  padding: 15px;
+  width: 500px;
+  height: 650px;
+  border-radius: 5px;
+  background: ${(props) => props.theme.text};
+  color: ${({ theme }) => theme.background};
+  box-shadow: 10px 5px 10px rgba(111, 111, 111, 0.5);
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background: rgba(111, 111, 111, 0.5);
+  padding: 15px;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius: 3px;
+  width: 100%;
+  display: flex;
+`;
+
+const Content = styled.div`
+  padding: 20px;
+  font-weight: 600;
+  display: flex;
+  flex-direction: column;
+  background: rgba(111, 111, 111, 0.5);
+  border-radius: 3px;
+  height: 100%;
+  gap: 10px;
+`;
