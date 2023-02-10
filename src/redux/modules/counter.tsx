710af2a063e5732d6373770ae51e859a91bb2773
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-02-10 15:23:58 +0900

    feat: Redux 디렉토리 추가

---
+++ src/redux/modules/counter.tsx
@@ -0,0 +1,54 @@
+// /* 0. Action Type */
+// const INCREASE = 'counter/INCREASE' as const;
+// const DECREASE = 'counter/DECREASE' as const;
+// const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+/* Action types */
+import ErrorPage from './src/pages/Error';
+
+const INCREASE = 'couter/INCREASE' as const;
+const DECREASE = 'counter/DECREASE' as const;
+const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseBy = (amount: number) => ({
+  type: INCREASE_BY,
+  payload: amount,
+});
+
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+/* 1. Initial State */
+
+export type CounterState = {
+  counter: number;
+};
+
+const initialState: CounterState = {
+  counter: 0,
+};
+
+/* 2. Reducer : state를 action의 type에 따라 바꾸는 함수 */
+const counter = (state: CounterState = initialState, action: CounterAction) => {
+  const { counter } = state;
+
+  switch (action.type) {
+    case INCREASE:
+      return { counter: counter + 1 };
+
+    case DECREASE:
+      return { counter: counter - 1 };
+
+    case INCREASE_BY:
+      return { counter: state.counter + action.payload };
+
+    default:
+      return state;
+  }
+};
+
+export default counter;
